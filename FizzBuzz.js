d2256f0842507599e014bae8df983bbb17f628d1
nknandakuamr <tec.thunder2022nk@gmail.com>
2024-11-21 11:40:58 +0530

    FizzBuzz Challenge

---
+++ FizzBuzz.js
@@ -0,0 +1,25 @@
+    const fizbuzz =(n)=>{
+   
+        if(n%3 == 0 && n%5 == 0){
+             console.log('FizzBuzz');
+        }
+    
+       else if (n%3 ==0) {
+            console.log('Fizz');
+        }
+       else if (n%5 == 0) {
+            console.log('Buzz');
+        } 
+        else{
+            console.log(n)
+        }
+       
+           
+        
+    }
+
+  
+
+for (let n = 1; n < 50; n++) {
+    fizbuzz(n);
+}
