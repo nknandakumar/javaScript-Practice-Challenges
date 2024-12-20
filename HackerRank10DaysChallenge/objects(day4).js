980fc5156787d2b9e8971ade82fdb61c1bbfda20
nknandakuamr <tec.thunder2022nk@gmail.com>
2024-12-20 19:01:25 +0530

    object DAY4

---
+++ HackerRank10DaysChallenge/objects(day4).js
@@ -0,0 +1,12 @@
+function Rectangle(a, b) {
+    const data = {
+        length : a ,
+        width : b ,
+        perimeter : 2*a+2*b ,
+        area : a*b
+    }
+        return data
+    }
+
+    console.log(Rectangle(5,5));
+    
